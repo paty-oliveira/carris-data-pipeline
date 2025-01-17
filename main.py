ce11da6eecf622e76a63a46226cb2c74c4f2e37b
Patricia Oliveira <patricia.oliveira@mindera.com>
2025-01-17 14:41:07 +0000

    Mount GCP keys in the Container

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 from dotenv import load_dotenv
 import os
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+CREDENTIALS_PATH = os.getenv("SERVICE_ACCOUNT_FILE")
+DATASET_ID = os.getenv("DATASET_ID")
 
 
 def get_json_data(endpoint):
@@ -16,14 +21,32 @@ def get_json_data(endpoint):
         print(f"Error in API call - {error}")
 
 
+def load_table_from_json(db_client, table_id, data):
+    try:
+        db_client.load_table_from_json(data, destination=table_id)
+        print(f"Created and loaded table: {table_id} with {len(data)} rows")
+
+    except Exception as error:
+        print(f"Error during loading data in db - {error}")
+
+
+def new_biquery_client():
+    credentials = service_account.Credentials.from_service_account_file(
+        CREDENTIALS_PATH
+    )
+
+    return bigquery.Client(credentials=credentials)
+
+
 def main():
     endpoint = "https://api.carrismetropolitana.pt/"
+    db_client = new_biquery_client()
 
-    vehicles_data = get_json_data(endpoint=endpoint + "vehicles")
-    print(vehicles_data)
-
-    municipalities_data = get_json_data(endpoint=endpoint + "municipalities")
-    print(municipalities_data)
+    municipalities_param = "municipalities"
+    municipalities_data = get_json_data(endpoint=endpoint + municipalities_param)
+    load_table_from_json(
+        db_client, f"{DATASET_ID}.{municipalities_param}", municipalities_data
+    )
 
 
 if __name__ == "__main__":
